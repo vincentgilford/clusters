74d52047bd7e85c0271ecd2de7f2f9a9a2510f74
Gilford <vincentgilford@gmail.com>
2018-02-01 20:17:54 -0500

    3 Tests done

--- src/main/java/clusters/Clusters.java
+++ src/main/java/clusters/Clusters.java
@@ -1,5 +1,33 @@
 package clusters;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class Clusters {
 
+	private int[] numbers;
+	
+	public Clusters(int[]  numbers) {
+		// TODO Auto-generated constructor stub
+		this.numbers = numbers; 
+	}
+
+	public int findCluster() {
+	// TODO Auto-generated method stub
+	int count = 0; 
+	for(int i =0; i<numbers.length-3; i++) {
+		if(numbers[i] == numbers[i+1]) {
+			count++;
+//			i+=3;	
+			} 
+			
+		}
+			
+
+		return count; 
+	}
+	
+	
+	
+	
 }
--- src/test/java/clusters/ClustersTest.java
+++ src/test/java/clusters/ClustersTest.java
@@ -1,10 +1,73 @@
 package clusters;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+ 
+import java.util.Arrays;
+import java.util.Collection;
+
+
+import org.junit.Test;
+import org.hamcrest.*;
+
+
 
 public class ClustersTest {
+	@Test
+	public void testForZero() {
+		//arrange
+		int[] numbers = {1,2,3};
+		Clusters underTest = new Clusters(numbers); 
+		//act
+		underTest.findCluster();
+		int check = 0;
+				
+		//assert
+		assertEquals(underTest.findCluster(),(check)); 
+		assertThat(underTest.findCluster(),is(check));
+	}
+
+	@Test
+	public void testForZeroWithParameters() {
+		//arrange
+		int[] numbers = {1,2,3};
+		Clusters underTest = new Clusters(numbers);  
+		int check = 0; 
+		//act
+		underTest.findCluster();
+		
+		
+		//assert
+		assertEquals(check, underTest.findCluster());
+	}
 	
+	@Test
+	public void testForOneCluster() {
+		//arrange
+		int[] numbers = {5,2,2,2,4};
+		Clusters underTest = new Clusters(numbers);
+		
+		//act
+		underTest.findCluster();
+		int check = 1; 
+		
+		//assert
+		assertThat(underTest.findCluster(), is(check));
+		
+	}
 	
-	
-	
-	
+	@Test
+	public void testForOneClusterAgain() {
+		//arrange
+		int[] numbers = {1,4,4,4,9,6,6,7};
+		Clusters underTest = new Clusters(numbers);
+		
+		//act
+		underTest.findCluster();
+		int check = 2; 
+		
+		//assert
+		assertThat(underTest.findCluster(), is(check));
+		
+	}
 }
